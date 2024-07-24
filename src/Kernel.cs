7afcd5f61368b8e1ba77bcf6a42dd64a441e3f43
P€pperMCGamer$ <113534696+BoomX64@users.noreply.github.com>
2024-07-24 19:26:26 +0700

    Update Kernel.cs

--- src/Kernel.cs
+++ src/Kernel.cs
@@ -2,7 +2,11 @@ using System;
 using Sys = Cosmos.System;
 
 public class Kernel : Sys.Kernel {
-  protected static void run() {}
+  protected override void BeforeRun() {
+    Console.Writeline("Welcome to KawaiiOS! Type Help To Show All The Command List.");
+  }
+
+  protected override void Run() {}
 }
 
 
