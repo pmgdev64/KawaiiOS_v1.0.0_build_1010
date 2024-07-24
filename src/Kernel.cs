8a68993915775e11842af41b67332c966975b8f3
P€pperMCGamer$ <113534696+BoomX64@users.noreply.github.com>
2024-07-24 19:33:39 +0700

    Update Kernel.cs

--- src/Kernel.cs
+++ src/Kernel.cs
@@ -6,7 +6,35 @@ public class Kernel : Sys.Kernel {
     Console.Writeline("Welcome to KawaiiOS! Type Help To Show All The Command List.");
   }
 
-  protected override void Run() {}
+  protected override void Run() {
+        Console.Write($"{_Prompt}> ");
+        var input = Console.ReadLine();
+        string[] words = input.Split(' ');
+        switch (words[0]) {
+            case "cpu":
+                Console.WriteLine($"Vendor: {CPU.GetCPUVendorName()}, Name: {CPU.GetCPUBrandString()}, Frequency: {CPU.GetCPUCycleSpeed()}");
+                break;
+            case "shutdown":
+                Sys.Power.Shutdown(); // shutdown is supported
+                break;
+            case "restart":
+                Sys.Power.Reboot(); // restart too
+                break;
+            case "help":
+                // console methods are plugged
+                Console Writeline("<Help Page>");
+                Console.WriteLine("cpu      - prints info about current cpu");
+                Console.WriteLine("shutdown - shuts down current computer");
+                Console.WriteLine("restart  - restarts current computer");
+                Console.WriteLine("help     - shows this help menu");
+                Console.Writeline("install  - install the system into hard disk");
+                break;
+            default:
+                // switch operator works great
+                Console.WriteLine($"\"{words[0]}\" is not a command");
+                break;
+        }
+  }
 }
 
 
