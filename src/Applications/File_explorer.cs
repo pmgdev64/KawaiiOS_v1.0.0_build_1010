4134d3ba055249df23bb2775eb754cf51eaa912b
P€pperMCGamer$ <113534696+pmgdev64@users.noreply.github.com>
2024-08-23 20:57:10 +0700

    Update File_explorer.cs

--- src/Applications/File_explorer.cs
+++ src/Applications/File_explorer.cs
@@ -3,5 +3,17 @@ using System.Drawing;
 
 namespace kawaiios.application.file_explorer 
 {
-  class File_explorer:App {}
+  class File_explorer:App {
+      int textEachLine;
+      public string text = string.Empty;
+
+      public Notepad(uint width, uint height, uint x = 0, uint y = 0) : base(width, height, x, y)
+        {
+            //ASC16 = 16*8
+            textEachLine = (int)width / 8;
+            name = "File Explorer";
+        }
+
+      public override void _Update() {}
+  }
 }
